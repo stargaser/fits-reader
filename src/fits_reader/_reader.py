fd384f8c71752ce6e75cf7e8e348fba8f79020ed
David Shupe <shupe@ipac.caltech.edu>
2022-07-16 16:18:34 -0500

    first working version of fits reading

--- src/fits_reader/_reader.py
+++ src/fits_reader/_reader.py
@@ -6,6 +6,7 @@ implement multiple readers or even other plugin contributions. see:
 https://napari.org/plugins/guides.html?#readers
 """
 import numpy as np
+from astropy.io import fits
 
 
 def napari_get_reader(path):
@@ -29,7 +30,7 @@ def napari_get_reader(path):
         path = path[0]
 
     # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+    if not path.endswith(".fits"):
         return None
 
     # otherwise we return the *function* that can read ``path``.
@@ -61,7 +62,7 @@ def reader_function(path):
     # handle both a string and a list of strings
     paths = [path] if isinstance(path, str) else path
     # load all files into array
-    arrays = [np.load(_path) for _path in paths]
+    arrays = [fits.getdata(_path) for _path in paths]
     # stack arrays into single array
     data = np.squeeze(np.stack(arrays))
 
